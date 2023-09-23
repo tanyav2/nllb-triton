4bd188ce4826ec14d22f5a3225566fadde11310e
tanyav2 <vermatanyax@gmail.com>
2023-09-22 18:10:33 -0700

    dosd

--- model_repository/m2m100-1.2b/1/model.py
+++ model_repository/m2m100-1.2b/1/model.py
@@ -36,95 +36,66 @@ class TritonPythonModel:
         script_dir = Path(__file__).parent
         self.model_dir = script_dir / "huggingface-model-cache"
         self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_dir).cuda(0)
-
-        # Dict to store tokenizers for different source languages. English tokenizer is preloaded.
-        self.tokenizers = {"en": AutoTokenizer.from_pretrained(self.model_dir, src_lang="en")}
-        self.valid_lang_codes = {'ja', 'oc', 'jv', 'ig', 'uz', 'vi', 'is', 'sv', 'ba', 'fa', 'ar', 'ast', 'id', 'post 1500', 'ne', 'ff', 'af', 'gl', 'mr', 'pa', 'lb', 'fy', 'hu', 'kn', 'my', 'ceb', 'pl', 'bg', 'ps', 'si', 'ms', 'ns', 'hy', 'am', 'ru', 'ta', 'lo', 'sq', 'sl', 'tl', 'cy', 'nl', 'ml', 'mg', 'so', 'mn', 'sr', 'sk', 'en', 'sw', 'uk', 'hi', 'el', 'zh', 'tr', 'it', 'ln', 'lt', 'ka', 'no', 'he', 'sd', 'su', 'or', 'km', 'ro', 'ha', 'zu', 'gu', 'kk', 'cs', 'bn', 'ga', 'pt', 'lg', 'ur', 'br', 'bs', 'da', 'ko', 'yi', 'hr', 'fi', 'az', 'ilo', 'et', 'ht', 'ca', 'ss', 'mk', 'th', 'xh', 'yo', 'de', 'fr', 'lv', 'gd', 'es', 'tn', 'be', 'wo'}
+        self.valid_langs = {
+            "en", "es", "fr", "de", "it", "pt", "nl", "ru", "zh", "ja", "ko", "ar",
+            "hi", "bn", "tr", "sv", "el", "fi", "pl", "vi", "he"
+        }
+        self.tokenizers = {
+            lang: AutoTokenizer.from_pretrained(self.model_dir, src_lang=lang)
+            for lang in self.valid_langs
+        }
 
 
     def execute(self, requests):
         responses = []
         for request in requests:
-            input_batch = pb_utils.get_input_tensor_by_name(request, "input").as_numpy()
-            tgt_langs = pb_utils.get_input_tensor_by_name(
-                request, "tgt_lang"
-            ).as_numpy()
-            src_langs = pb_utils.get_input_tensor_by_name(
-                request, "src_lang"
-            ).as_numpy()
+            inputs = pb_utils.get_input_tensor_by_name(request, "input").as_numpy()
+            srcs = pb_utils.get_input_tensor_by_name(request, "src_lang").as_numpy()
+            tgts = pb_utils.get_input_tensor_by_name(request, "tgt_lang").as_numpy()
 
-            input_texts_by_src = {}
-            tgt_langs_list = []
-            for input_bytes, tgt_lang_bytes, src_lang_bytes in zip(
-                input_batch, tgt_langs, src_langs
-            ):
-                input_text = input_bytes[0].decode()
-                tgt_lang = tgt_lang_bytes[0].decode()
-                src_lang = src_lang_bytes[0].decode()
-
-                if src_lang not in self.valid_lang_codes:
-                    logger.error(f"Invalid src_lang: {src_lang}. Skipping translation.")
+            texts_by_src, tgt_list, errors = {}, [], {}
+            for i, (in_data, src, tgt) in enumerate(zip(inputs, srcs, tgts)):
+                s, t = src[0].decode(), tgt[0].decode()
+                if s not in self.valid_langs:
+                    errors[i] = f"ERROR: invalid src lang ({s})"
                     continue
-
-                if tgt_lang not in self.valid_lang_codes:
-                    logger.error(f"Invalid tgt_lang: {tgt_lang}. Skipping translation.")
+                if t not in self.valid_langs:
+                    errors[i] = f"ERROR: invalid tgt lang ({t})"
                     continue
-
-                if src_lang not in input_texts_by_src:
-                    input_texts_by_src[src_lang] = []
-
-                input_texts_by_src[src_lang].append(input_text)
-                tgt_langs_list.append(tgt_lang)
+                texts_by_src.setdefault(s, []).append((in_data[0].decode(), i))
+                tgt_list.append(t)
 
             # Tokenize by src_lang and then combine
-            tokenized_inputs = []
-            for src_lang, input_texts in input_texts_by_src.items():
-                if src_lang not in self.tokenizers:
-                    self.tokenizers[src_lang] = AutoTokenizer.from_pretrained(
-                        self.model_dir, src_lang=src_lang
-                    )
-
-                tokenizer = self.tokenizers[src_lang]
-                inputs = tokenizer(
-                    input_texts, padding="longest", return_tensors="pt"
-                ).to(0)
-                tokenized_inputs.append(inputs)
-
-            # Find max length and pad accordingly
-            max_length = max([i['input_ids'].shape[1] for i in tokenized_inputs])
-            input_ids, attention_mask = zip(*[
-                (
-                    torch.cat(
-                        [i['input_ids'], torch.zeros(i['input_ids'].shape[0], max_length - i['input_ids'].shape[1]).type(torch.long).to(0)],
-                        dim=1
-                    ),
-                    torch.cat(
-                        [i['attention_mask'], torch.zeros(i['input_ids'].shape[0], max_length - i['input_ids'].shape[1]).type(torch.long).to(0)],
-                        dim=1
-                    ),
-                )
-                for i in tokenized_inputs
+            tokenized, order_map = [], []
+            for src, texts in texts_by_src.items():
+                in_data, idxs = zip(*texts)
+                toks = self.tokenizers[src](list(in_data), padding="longest", return_tensors="pt").to(0)
+                tokenized.append(toks)
+                order_map.extend(idxs)
+            
+            max_len = max(inp['input_ids'].shape[1] for inp in tokenized)
+            ids = torch.cat([
+                torch.nn.functional.pad(inp['input_ids'], (0, max_len - inp['input_ids'].shape[1]))
+                for inp in tokenized
             ])
-            input_ids, attention_mask = torch.cat(input_ids, dim=0), torch.cat(attention_mask, dim=0)
+            masks = torch.cat([
+                torch.nn.functional.pad(inp['attention_mask'], (0, max_len - inp['attention_mask'].shape[1]))
+                for inp in tokenized
+            ])
 
-            # assuming default tokenizer for getting correct lang id
-            bos_token_ids = [self.tokenizers["en"].get_lang_id(tgt_lang) for tgt_lang in tgt_langs_list]
+            bos_ids = [self.tokenizers["en"].get_lang_id(t) for t in tgt_list]
 
             translated_tokens = self.model.generate(
-                input_ids=input_ids, 
-                attention_mask=attention_mask,
-                forced_bos_token_id=torch.LongTensor(bos_token_ids)
+                input_ids=ids, 
+                attention_mask=masks,
+                forced_bos_token_id=torch.LongTensor(bos_ids)
             )
-
-            # assuming default tokenizer for decoding
             translations = self.tokenizers["en"].batch_decode(translated_tokens, skip_special_tokens=True)
+            translations = [x for _, x in sorted(zip(order_map, translations), key=lambda pair: pair[0])]
+            for i, err in errors.items():
+                translations.insert(i, err)
 
-            output_tensor = pb_utils.Tensor(
-                "translation", np.array(translations, dtype=object)
-            )
-            inference_response = pb_utils.InferenceResponse(
-                output_tensors=[output_tensor]
-            )
-            responses.append(inference_response)
+            out_tensor = pb_utils.Tensor("translation", np.array(translations, dtype=object))
+            responses.append(pb_utils.InferenceResponse(output_tensors=[out_tensor]))
 
         return responses
